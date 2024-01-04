6e826adb91b8d049d4be99cfe6e6cb4f94d8aadf
Hnem <149763377+Hnem12@users.noreply.github.com>
2024-01-04 20:15:56 +0700

    Update sever.js

--- sever.js
+++ sever.js
@@ -2,19 +2,23 @@ const express = require('express')
 const app = express()
 var bodyParser = require('body-parser')
 const AccountModel = require('./models/account')
+const DanhmucModel = require('./models/danhmuc')
+const SanphamModel= require('./models/sanpham')
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-var router = require('./apiRouter')
-var danhmuctRouter= require('./danhmuc')
+
+var router = require('./router/apiRouter')
+app.use('/api/account/', accountRouter)
+
+var danhmuctRouter= require('./router/danhmuc')
 app.use('/api/danhmuc/', danhmuctRouter)
 
-var sanphamRouter= require('./sanpham')
+var sanphamRouter= require('./router/sanpham')
 app.use('/api/sanpham/', sanphamRouter)
 
-app.use('/api/account', router)
 
 
 app.get('/', (req, res, next) =>{   
